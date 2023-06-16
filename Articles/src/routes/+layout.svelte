58de01e3ceb24bc0d54804b3d2cada408aef78ff
Israel Joseph <israel-joseph@hotmail.com>
2023-06-15 22:22:25 -0300

    Criação da página de artigos

---
+++ Articles/src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<header>
+	<a class="item_menu link" href="/">Home Page</a>
+</header>
+
+<div class="main">
+	<slot />
+</div>
+
+<footer>
+    <p>Criado por Israel Joseph</p>
+</footer>
+
+<style>
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 50px;
+		background-color: #00aeff;
+        color: #ffffff;
+	}
+
+	.item_menu {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 120px;
+		height: 100%;
+		padding: 0px 5px;
+		margin: 0px 5px;
+        color: #ffffff;
+	}
+
+	.link {
+		text-decoration: none;
+		font-size: 1.2rem;
+	}
+
+	.link:hover {
+		background-color: #066896;
+		transition: all 2s;
+		text-decoration: none;
+	}
+
+	.main {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+    footer {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 50px;
+		background-color: #00aeff;
+        color: #ffffff;
+        font-size: large;
+	}
+</style>
---
+++ Articles/src/routes/+page.svelte
@@ -0,0 +1,118 @@
+<div class="container">
+	<section>
+		<div class="title">
+			<h1>Bem-vindo(a) a página inicial</h1>
+		</div>
+		<hr style="color: #ffffff" />
+	</section>
+
+	<section>
+		<div class="title">
+			<h2>Artigos</h2>
+		</div>
+		<div class="articles">
+			<div class="article">
+				<div class="article_title">
+					<h3>Artigo 1</h3>
+				</div>
+				<div class="article_content">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+						has been the industry's standard dummy text ever since the 1500s, when an unknown
+						printer took a galley of type and scrambled it to make a type specimen book. It has
+						survived not only five centuries, but also the leap into electronic typesetting,
+						remaining essentially unchanged. It was popularised in the 1960s with the release of
+						Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+						publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+				</div>
+				<div class="article_link">
+					<a href="/article1">Read More...</a>
+				</div>
+			</div>
+			<div class="article">
+				<div class="article_title">
+					<h3>Artigo 2</h3>
+				</div>
+				<div class="article_content">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+						has been the industry's standard dummy text ever since the 1500s, when an unknown
+						printer took a galley of type and scrambled it to make a type specimen book. It has
+						survived not only five centuries, but also the leap into electronic typesetting,
+						remaining essentially unchanged. It was popularised in the 1960s with the release of
+						Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+						publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+				</div>
+				<div class="article_link">
+					<a href="/article2">Read More...</a>
+				</div>
+			</div>
+			<div class="article">
+				<div class="article_title">
+					<h3>Artigo 3</h3>
+				</div>
+				<div class="article_content">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+						has been the industry's standard dummy text ever since the 1500s, when an unknown
+						printer took a galley of type and scrambled it to make a type specimen book. It has
+						survived not only five centuries, but also the leap into electronic typesetting,
+						remaining essentially unchanged. It was popularised in the 1960s with the release of
+						Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+						publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+				</div>
+				<div class="article_link">
+					<a href="/article3">Read More...</a>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+<style>
+	.container {
+		display: grid;
+		width: 100%;
+	}
+
+	section {
+		width: 100%;
+	}
+
+	section > .title {
+		font-size: 1.4rem;
+		padding-left: 50px;
+	}
+
+	.articles {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 1.2rem;
+		padding: 0px 10px;
+		margin-bottom: 40px;
+	}
+
+	.article {
+		padding: 0px 20px;
+	}
+
+	.article_title {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.article_content {
+		text-align: justify;
+		font-size: 1rem;
+	}
+
+	.article_link {
+		text-align: center;
+		font-size: 1rem;
+	}
+</style>
