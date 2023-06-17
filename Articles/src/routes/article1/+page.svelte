f89bf1ee521681dba921491c74cd258b7e1fc8c8
Israel Joseph <israel-joseph@hotmail.com>
2023-06-17 14:45:51 -0300

    Criação das páginas dos artigos e seu conteúdo

--- Articles/src/routes/article1/+page.svelte
+++ Articles/src/routes/article1/+page.svelte
@@ -1,1 +1,130 @@
-<h1>Artigo 1</h1>
+<div class="container">
+	<div class="page_title">
+		<h1>Artigo 1</h1>
+	</div>
+	<div class="article_title">
+		<h2>W3C anuncia medida de padronização para a Confirmação de Pagamento Seguro</h2>
+	</div>
+	<div class="article_content">
+		<div>
+			<img
+				class="article_image"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/W3C%C2%AE_Icon.svg/1200px-W3C%C2%AE_Icon.svg.png"
+				alt="W3c Logo"
+			/>
+		</div>
+		<p>
+			O World Wide Web Consortium anunciou uma nova medida de padronização para a confirmação de
+			pagamento seguro, que ajudará a simplificar a autenticação do usuário e aumentar a segurança
+			do pagamento durante o processo de checkout na web.
+		</p>
+		<p>
+			O World Wide Web Consortium (W3C) é um órgão sem fins lucrativos que estabelece padrões
+			técnicos da Web e aprova as diretrizes que regem HTML e CSS.
+		</p>
+		<p>
+			O Secure Payment Confirmation (SPC) é uma API da Web destinada a permitir que comerciantes,
+			bancos, provedores de serviços de pagamento e redes de cartões reduzam o atrito da
+			autenticação forte do cliente (SCA) e produzam evidências criptográficas do consentimento do
+			usuário.
+		</p>
+		<p>
+			A SPC adiciona uma etapa extra ao processo de pagamento que chama de “consentimento do
+			usuário”, solicitando que os usuários revisem e confirmem os detalhes do pagamento por meio de
+			uma caixa de diálogo fornecida pelo navegador.
+		</p>
+		<p>
+			O SPC pode habilitar pagamentos via biometria. Por exemplo, depois de optar por comprar
+			mercadorias, os usuários poderão confirmar as transações por meio do FaceID do iPhone, usando
+			o protocolo WebAuthn fornecido pela FIDO Alliance.
+		</p>
+		<p>
+			Essa forte autenticação do cliente e evidência criptográfica são requisitos regulatórios, como
+			a Diretiva de Serviços de Pagamento (PSD2) na Europa.
+		</p>
+		<p>
+			Lançada em 2017, a PSD2 era uma diretiva da UE destinada a tornar os pagamentos mais seguros e
+			menos propensos a fraudes, que começou a ser implementada em 2018.
+		</p>
+		<p>
+			De acordo com o consórcio, a publicação da confirmação de pagamento seguro como uma
+			recomendação de candidato indica que o conjunto de recursos é estável e recebeu ampla revisão.
+		</p>
+		<p>
+			O W3C disse que buscará experiência de implementação adicional antes de avançar esta versão da
+			Confirmação de Pagamento Seguro para a recomendação.
+		</p>
+		<p>Em 2019, o consórcio começou a trabalhar na confirmação de pagamento seguro.</p>
+		<p>
+			Em termos de aplicativos do mundo real, a gigante multinacional de processamento de pagamentos
+			Stripe conduziu um piloto com uma implementação antecipada do SPC em março de 2020.
+		</p>
+		<p>
+			A Stripe relatou que, em comparação com as senhas únicas (OTP), a autenticação SPC levou a um
+			aumento de 8% nas conversões e que o checkout foi 3 vezes mais rápido.
+		</p>
+		<p>
+			O World Wide Web Consortium também destacou o caso de uso da tecnologia fora dos pagamentos
+			com cartão tradicionais, dizendo que está discutindo como o SPC pode ser integrado a outros
+			ecossistemas de pagamento, como Open Banking e PIX, a rede de pagamentos do Brasil, bem como
+			em pagamentos proprietários.
+		</p>
+		<p>
+			O SPC está atualmente disponível no Chrome e Edge no MacOS, Windows e Android, e o Web
+			Payments Working Group diz que buscará implementação em outros navegadores e ambientes durante
+			o período de Recomendação do Candidato.
+		</p>
+		<div>
+			<p class="article_reference">
+				The article is from
+				<a
+					href="https://cryptoid.com.br/mercado-financeiro/w3c-anuncia-medida-de-padronizacao-para-a-confirmacao-de-pagamento-seguro/"
+					target="_blank"
+				>
+					cryptoid.com.br
+				</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: grid;
+		width: 100%;
+		max-width: 1280px;
+	}
+	.page_title {
+		font-size: 1.4rem;
+		padding-left: 50px;
+	}
+
+	.article_title {
+		display: flex;
+		/* justify-content: center; */
+		margin: 0px 20px;
+		align-items: center;
+		color: rgb(0, 0, 0);
+		font-size: larger;
+	}
+	.article_content {
+		margin: 0px 20px 40px 20px;
+		text-align: justify;
+	}
+	.article_image {
+		width: 400px;
+		height: 300px;
+	}
+	.article_content > p {
+		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	}
+	.article_reference > a {
+		/* background-color: bisque; */
+		transition: all 1s;
+	}
+	.article_reference > a:hover {
+		color: rgb(62, 196, 201);
+		font-size: large;
+		transition: all 1s;
+	}
+</style>
--- Articles/src/routes/article3/+page.svelte
+++ Articles/src/routes/article3/+page.svelte
@@ -1,1 +1,196 @@
-<h1>Artigo 3</h1>
+<div class="container">
+	<div class="page_title">
+		<h1>Artigo 3</h1>
+	</div>
+	<div class="article_title">
+		<h2>GitHub finds 92% of developers love AI tools</h2>
+	</div>
+	<div class="article_content">
+		<div>
+			<img
+				class="article_image"
+				src="https://regmedia.co.uk/2019/07/20/shutterstock_programmer.jpg?x=302&y=151&crop=1"
+				alt="IA"
+			/>
+		</div>
+		<p>
+			GitHub, which has been doing a brisk business selling subscriptions to its
+			litigation-encumbered Copilot AI helper, surveyed 500 US-based developers to find if any of
+			them are already using AI coding tools at work.
+		</p>
+		<p>As it turns out, they are, and in a big way. We know, you're shocked.</p>
+		<p>
+			The Microsoft-owned code storage biz recently hired Wakefield Research to interrogate coders
+			at companies with more than 1,000 employees about their tools and workflows. The purpose of
+			the survey, according to chief product officer, Inbal Shani, is to learn more about
+			productivity, collaboration, AI, and operating effectively in enterprise environments.
+		</p>
+		<p>
+			Among the key findings of the report released on Tuesday is that developers have already
+			committed to AI assistance, evidently heedless of
+			<a href="https://www.theregister.com/2023/06/09/github_copilot_lawsuit/" target="_blank">
+				legal challenges
+			</a>
+			that could limit such tools on copyright grounds.
+		</p>
+		<p>
+			<i>
+				"A staggering 92 percent of US-based developers working in large companies report using an
+				AI coding tool either at work or in their personal time – and 70 percent say they see
+				significant benefits to using these tools",
+			</i>
+			the study says.
+		</p>
+		<p>
+			Interestingly, the report indicates that developers believe AI tools will improve the code
+			they create.
+		</p>
+		<p>
+			<i>
+				"Developers say AI coding tools can help them meet existing performance standards with
+				improved code quality, faster outputs, and fewer production-level incidents"
+			</i>
+			the survey says.
+		</p>
+		<p>
+			Respondents may not be aware of academic studies to the contrary. One, for example, found AI
+			helpers like ChatGPT produce code that is
+			<i>
+				"<a href="https://www.theregister.com/2023/04/21/chatgpt_insecure_code/" target="_blank">
+					well below
+				</a>
+				minimal security standards applicable in most contexts."
+			</i>
+			Another found that Copilot
+			<a href="https://www.theregister.com/2022/12/21/ai_assistants_bad_code/" target="_blank">
+				produced more security vulnerabilities
+			</a>
+			than code created without AI help, while developers mistakenly thought the machine-learning tools
+			produced fewer mistakes. A third study found Copilot produced security bugs
+			<a href="https://www.theregister.com/2021/08/25/github_copilot_study/" target="_blank">
+				about 40 percent
+			</a>
+			of the time.
+		</p>
+		<p>
+			Fittingly, those responding to GitHub's survey questions appear to believe that less weight
+			should be given to code quality as a performance metric. Asked how they are currently judged,
+			these programmers responded: code quality (40 percent); time to complete a task (34 percent);
+			number of production incidents (34 percent); lines of code written (33 percent); and number of
+			bugs or issues resolved (33 percent).
+		</p>
+		<p>
+			Assuming AI coding tools are used, these devs would prefer to be judged on the basis of: code
+			quality (36 percent); time to complete a task (36 percent); number of production incidents (33
+			percent); lines of code written (33 percent); and number of pull requests (32 percent).
+		</p>
+		<p>
+			Speedier and shoddier seems to be the order of the day. And in a further example of
+			self-interest, the same portion of devs who say they're judged on lines of code written
+			believe that metric should continue, even under the presumed productivity enhancement of an AI
+			helper.
+		</p>
+		<p>
+			GitHub's
+			<a
+				href="https://github.blog/2023-06-13-survey-reveals-ais-impact-on-the-developer-experience/"
+				target="_blank"
+			>
+				survey
+			</a>
+			suggests this may be worth reconsidering:
+			<i>
+				"With the increase of AI tooling being used in software development – which often
+				contributes to code volume – engineering leaders will need to ask whether measuring code
+				volume is still the best way to measure productivity and output."
+			</i>
+		</p>
+		<p>
+			Many of the surveyed devs (81 percent) also see AI as a path toward greater collaboration
+			within teams and organizations. Asked where teams armed with AI tools should focus, they said:
+			code reviews (45 percent); security reviews (45 percent); planning (43 percent); pair
+			programming (43 percent); solution design (42 percent).
+		</p>
+		<p>
+			When questioned on how AI tools can help their daily work, the devs said: develop your
+			language coding skills (57 percent); become more productive (53 percent); focus on
+			building/creating vs. repetitive tasks (51 percent); and prevent burnout (41 percent).
+		</p>
+		<p>
+			The survey concludes,
+			<i>
+				"Developer satisfaction, productivity, and organizational impact are all positioned to get a
+				boost from AI coding tools – and that will have a material impact on the overall developer
+				experience."
+			</i>
+			Just wait until you hear how much writers adore autocorrect and spell check.
+		</p>
+		<div>
+			<p class="article_reference">
+				The article is from
+				<a href="https://www.theregister.com/2023/06/13/github_developers_ai/" target="_blank"
+					>The Register
+				</a>.
+			</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: grid;
+		width: 100%;
+		max-width: 1280px;
+	}
+	.page_title {
+		font-size: 1.4rem;
+		padding-left: 50px;
+	}
+
+	.article_title {
+		display: flex;
+		/* justify-content: center; */
+		margin: 0px 20px;
+		align-items: center;
+		color: rgb(0, 0, 0);
+		font-size: larger;
+	}
+	.article_content {
+		margin: 0px 20px 40px 20px;
+		text-align: justify;
+	}
+	.article_image {
+		width: 350px;
+		height: 200px;
+	}
+	.article_content > p {
+		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	}
+	.article_content > p > a {
+		/* background-color: bisque; */
+		transition: all 1s;
+	}
+	.article_content > p > a:hover {
+		color: rgb(62, 196, 201);
+		font-size: large;
+		transition: all 1s;
+	}
+	.article_content > p > i > a {
+		/* background-color: bisque; */
+		transition: all 1s;
+	}
+	.article_content > p > i > a:hover {
+		color: rgb(62, 196, 201);
+		font-size: large;
+		transition: all 1s;
+	}
+	.article_reference > a {
+		/* background-color: bisque; */
+		transition: all 1s;
+	}
+	.article_reference > a:hover {
+		color: rgb(62, 196, 201);
+		font-size: large;
+		transition: all 1s;
+	}
+</style>
